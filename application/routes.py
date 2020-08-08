973647988f55a80330a3b6dc46145d10fef7d154
Wasim <wasim.danyal1@gmail.com>
2020-08-08 18:33:17 +0100

    removed posts

--- application/routes.py
+++ application/routes.py
@@ -102,9 +102,6 @@ def account():
 @login_required
 def account_delete():
 		user = current_user.id
-		posts = Posts.query.filter_by(user_id=user)
-		for post in posts:
-				db.session.delete(post)
 		account = Users.query.filter_by(id=user).first()
 		logout_user()
 		db.session.delete(account)
